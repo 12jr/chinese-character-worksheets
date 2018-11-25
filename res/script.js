ef74fa328333133f36023f435c0463ec40617b4e
12jr <1203jr@gmx.de>
2018-11-25 20:01:05 +0100

    resolved #3, minor changes in design

--- res/script.js
+++ res/script.js
@@ -85,7 +85,7 @@ $(document).ready(function(){
 	*/
 	function createPdf(docTitle, characters, numberOfGrayscaleSigns, pasteSoImages, writePinyin, useGridlines, writeName, filename, charPicUrl, charPicBase64, charPicAvailable, charPinyin, wayOfRetrieval){
 		// make filename "filesystem-secure"
-			filename = filename.replace(/[^a-z0-9öäüß\-\_\u4E00-\u9FFF]/gi, '');
+			filename = filename.replace(/[^a-z0-9öäüß\s\-\_\u4E00-\u9FFF]/gi, '');
 			filename = filename == "" ? "my-chinese-exercise" : filename;
 		// notify user
 			$("#substatus").html("Starting to create your PDF");
@@ -98,7 +98,7 @@ $(document).ready(function(){
 			$("#substatus").html("Adding Pīnyīn font");
 			doc.addFileToVFS('NotoSans-Regular.ttf', pinyinFontBase64);
 			doc.addFont('NotoSans-Regular.ttf', 'Noto Sans', 'normal');
-		// set style of ends of lines (for nice corners)
+		// set style of ends of lines (for good looking corners)
 			doc.setLineCap("projecting");
 		// draw character lines: 11 characters per page, 11 characters per line
 			$("#substatus").html("Writing character lines (line <span class='amountDone'>0</span>/" + characters.length + ")");
@@ -119,7 +119,7 @@ $(document).ready(function(){
 						doc.addPage();
 						curPage++;
 					}
-					// top line
+					// line on top of the page
 					doc.line(xUpLeft, 17, 201, 17);
 					// write "姓名：" if the user wanted it
 					if(writeName){
@@ -137,16 +137,20 @@ $(document).ready(function(){
 				}
 				thisLineYUpLeft = yUpLeft + (i%charsPerPage) * charLineDistance;
 				// gridlines - part 1
-					// @TODO
+					if(useGridlines){
+						doc.setDrawColor("D0D0D0");
+						// horizontal lines for all of the characters in the character line
+						doc.line(	xUpLeft,
+									thisLineYUpLeft + 0.5 * charLineHeight,
+									xUpLeft + 11 * charCellWidth,
+									thisLineYUpLeft + 0.5 * charLineHeight);
+						doc.setDrawColor("000000");
+					}
 				// paste Wikimedia Stroke Order Images
 					if(pasteSoImages && charPicAvailable[i]) { // if user wants stroke order images AND if Wikimedia provides stroke order image
 						imgProp = doc.getImageProperties(charPicBase64[i]);
 						doc.addImage(charPicBase64[i], 'PNG', xUpLeft + 19, yUpLeft + (i%charsPerPage) * charLineDistance - 7, imgProp.width * 6.8 / imgProp.height, 6.8, "", "NONE", 0);
 					}
-				// upper line
-				doc.line(xUpLeft, thisLineYUpLeft, xUpLeft + 11 * charCellWidth, thisLineYUpLeft);
-				// lower line
-				doc.line(xUpLeft, thisLineYUpLeft + charLineHeight, xUpLeft + 11 * charCellWidth, thisLineYUpLeft + charLineHeight);
 				// write pinyin
 					if(writePinyin){
 						doc.setFont('Noto Sans');
@@ -158,7 +162,31 @@ $(document).ready(function(){
 				doc.setFontSize(40); //in pt
 				for(j = 0; j < 12; ++j){ // 11 chars per line
 					// gridlines - part 2
-						// @TODO
+						if(useGridlines){
+							doc.setDrawColor("D0D0D0");
+							// vertical line per character
+							doc.line(
+								xUpLeft + (j+0.5) * charCellWidth,	// x1
+								thisLineYUpLeft,					// y1
+								xUpLeft + (j+0.5) * charCellWidth,	// x2
+								thisLineYUpLeft + charLineHeight);	// y2
+							// 2 diagonal lines per character
+								doc.setLineCap("round");
+								// "⭸" left top --> right bottom
+								doc.line(
+									xUpLeft + j * charCellWidth,		// x1
+									thisLineYUpLeft,					// y1
+									xUpLeft + (j+1) * charCellWidth,	// x2
+									thisLineYUpLeft + charLineHeight);	// y2
+								// "⭷" left bottom --> right top
+								doc.line(
+									xUpLeft + j * charCellWidth,		// x1
+									thisLineYUpLeft + charLineHeight,	// y1
+									xUpLeft + (j+1) * charCellWidth,	// x2
+									thisLineYUpLeft);					// y2
+								doc.setLineCap("projecting");
+							doc.setDrawColor("000000");
+						}
 					// vertical lines
 					doc.line(xUpLeft + j * charCellWidth, thisLineYUpLeft, xUpLeft + j * charCellWidth, thisLineYUpLeft + charLineHeight);
 					// todo: write first char (j=0) and grey chars (j=1,...,11)
@@ -168,6 +196,10 @@ $(document).ready(function(){
 						doc.text(characters[i], xUpLeft + (j + 0.5) * charCellWidth, thisLineYUpLeft + charLineHeight - 3, 'center');
 					}
 				};
+				// upper line
+				doc.line(xUpLeft, thisLineYUpLeft, xUpLeft + 11 * charCellWidth, thisLineYUpLeft);
+				// lower line
+				doc.line(xUpLeft, thisLineYUpLeft + charLineHeight, xUpLeft + 11 * charCellWidth, thisLineYUpLeft + charLineHeight);
 				$(".amountDone").html(i);
 			};
 			
@@ -275,7 +307,6 @@ $(document).ready(function(){
 	*/
 	// Hide the div using jQuery rather than CSS, as if JavaScirpt is disabled
 	// the user should still be able to access the inputs.
-	$('#filenameTr').hide();
 	$('input:radio[name="way-of-retr"]').change(function() {
 		if (this.checked) {
 			if (this.value == 'download')
@@ -284,4 +315,6 @@ $(document).ready(function(){
 				$('#filenameTr').hide();
 		}
 	});
+	// check for the first time when html doc ready
+	$('input:radio[name="way-of-retr"]').change();
 });
